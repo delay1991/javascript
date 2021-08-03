0e0b9da37f94e54f0fc64e13de418c5c0ee665a1
DESKTOP-LAHJB9H\admin <chocopie4860@naver.com>
2021-08-03 17:04:27 +0900

    8/3 집에갈래 ㅠㅠ

--- welcomeHome1/src/main/java/co/yedam/board/CommentDAO.java
+++ welcomeHome1/src/main/java/co/yedam/board/CommentDAO.java
@@ -17,6 +17,88 @@ public class CommentDAO extends DAO {
 		return instance;
 	}
 
+	// 글내용수정
+	public HashMap<String, Object> update(Comments comment) {
+		connect();
+		String sql="update commemts set name=?, content=? where id=?";
+		try {
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, comment.getName());
+		pstmt.setString(2, comment.getContent());
+		pstmt.setString(3, comment.getId());
+		int r = pstmt.executeUpdate();
+		System.out.println("입력건수:" + r);
+		
+		HashMap<String, Object> map = new  HashMap<String, Object>();
+		map.put("id", comment.getId());
+		map.put("name", comment.getName());
+		map.put("content", comment.getContent());
+
+		return map;
+		
+		}catch(SQLException e){
+			e.printStackTrace();
+			
+		}finally {
+			disconnect();
+		}
+		return null;
+	}
+
+	// 글등록
+	public HashMap<String, Object> insert(Comments comment) {
+		System.out.println(comment);
+		// id_repository 테이블에서 현제 시퀀스번호
+		// comments 테이블추가
+		// id_repository에 새로운 시퀀스번호로 변경
+		int nextId = 0;
+		connect();
+		try {// 1 현재시퀀스번호 가져오고
+			conn.setAutoCommit(false);
+			stmt = conn.createStatement(); // psmt??와 기능 좀다름...
+			rs = stmt.executeQuery("select value from id_repository where name='comment'");
+			if (rs.next()) {
+				nextId = rs.getInt("value");
+			}
+			// 2 시퀀스1을 증가해서 comment 입력
+			nextId++;
+			pstmt = conn.prepareStatement("insert into commemts values(?,?,?)");
+			pstmt.setInt(1, nextId);
+			pstmt.setString(2, comment.getName());
+			pstmt.setString(3, comment.getContent());
+			int r = pstmt.executeUpdate(); // 조회, executeQuery, 입력수정삭제는 executeUpdate;
+			System.out.println("입력건수:" + r);
+			// 3 시퀀스번호 변경
+			pstmt = conn.prepareStatement("update id_repository set value=? where name='comment'");
+			pstmt.setInt(1, nextId);
+			pstmt.executeUpdate();
+
+			conn.commit();
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("id", nextId);
+			map.put("name", comment.getName());
+			map.put("content", comment.getContent());
+
+			return map;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				conn.rollback();
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				map.put("msg", e.getMessage());
+				return map;
+
+			} catch (SQLException e1) {
+				e.printStackTrace();
+			}
+
+		} finally {
+			disconnect();
+		}
+		return null;
+	}// 셀렉트,인서트했을때 에러나면 롤백할려고...?오라클기준 지금은 sqlite
+
 	// 글목록
 	public List<HashMap<String, Object>> selectAll() {
 		connect();
--- welcomeHome1/src/main/java/co/yedam/board/CommentServlet.java
+++ welcomeHome1/src/main/java/co/yedam/board/CommentServlet.java
@@ -29,36 +29,85 @@ public class CommentServlet extends HttpServlet {
 		String cmd = request.getParameter("cmd");
 
 		if (cmd == null) {
-			StringBuffer sb = new StringBuffer();
-			sb.append("<result>");
-			sb.append("<code>error</code>");
-			sb.append("<data>");
-			sb.append("cmd null");
-			sb.append("</data>");
-			sb.append("</result>");
-			out.print(sb.toString());
+			out.println(errorXML("cmd All"));
 
 		} else if (cmd.equals("selectAll")) {
-			List<HashMap<String, Object>> list = //
-					CommentDAO.getInstance().selectAll();
-			System.out.println(list.size());
-			StringBuffer sb = new StringBuffer();
-			sb.append("<result>");
-			sb.append("<code>success</code>");
-			for (HashMap<String, Object> map : list) {
-				sb.append("<data>");
-				sb.append("  <id>" + map.get("id") + "</id>");
-				sb.append("  <name>" + map.get("name") + "</name>");
-				sb.append("  <content>" + map.get("content") + "</content>");
-				sb.append("</data>");
-			}
-			sb.append("</result>");
-			System.out.println(sb.toString());
-			out.print(sb.toString());
-			
-		} else if (cmd.equals("insert")) {
+			try {
+				List<HashMap<String, Object>> list = // 전체입력
+						CommentDAO.getInstance().selectAll();
 
+				StringBuffer sb = new StringBuffer();
+				sb.append("<result>");
+				sb.append("<code>success</code>");
+				sb.append("<data>");
+				for (HashMap<String, Object> map : list) {
+					sb.append("<row>");
+					sb.append("  <id>" + map.get("id") + "</id>");
+					sb.append("  <name>" + map.get("name") + "</name>");
+					sb.append("  <content>" + map.get("content") + "</content>");
+					sb.append("</row>");
+				}
+				sb.append("</data>");
+				sb.append("</result>");
+				System.out.println(sb.toString());
+				out.print(sb.toString());
+
+			} catch (Exception e) {
+				out.println(errorXML(e.getMessage()));
+			}
+
+		} else if (cmd.equals("insert")) {// 한건입력
+			try {
+				String name = request.getParameter("name");
+				String content = request.getParameter("content");
+				Comments comment = new Comments();
+				comment.setName(name);
+				comment.setContent(content);
+				HashMap<String, Object> map = CommentDAO.getInstance().insert(comment);
+
+				out.println(dataXML(map));
+
+			} catch (Exception e) {
+				out.println(errorXML(e.getMessage()));
+			}
+
+		}else if(cmd.equals("update")) {
+			String id = request.getParameter("id");
+			String name = request.getParameter("name");
+			String content =request.getParameter("content");
+			Comments comment =new Comments();
+			comment.setId(id);
+			comment.setName(name);
+			comment.setContent(content);
+			
+			HashMap<String, Object> map=CommentDAO.getInstance().update(comment);
+			out.println(dataXML(map));
 		}
+		
+	}//end of doGet();
+	
+	private String dataXML(HashMap<String, Object> map) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<result>");
+		sb.append("<code>success</code>");
+		sb.append("<data>");
+		sb.append("  <id>" + map.get("id") + "</id>");
+		sb.append("  <name>" + map.get("name") + "</name>");
+		sb.append("  <content>" + map.get("content") + "</content>");
+		sb.append("</data>");
+		sb.append("</result>");
+		return sb.toString();
+	}
+	
+	private String errorXML(String msg) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<result>");
+		sb.append("<code>error</code>");
+		sb.append("<data>" + msg + "</data>");
+		sb.append("</result>");
+		
+		return sb.toString();
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
