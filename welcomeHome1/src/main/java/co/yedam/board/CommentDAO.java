b560386e3e55d41f32cd0ed95508961b4e66222a
DESKTOP-LAHJB9H\admin <chocopie4860@naver.com>
2021-08-05 17:06:42 +0900

    8/5 배열안에배열구조를 가져와서 구조를 바꿔보세요

--- welcomeHome1/src/main/java/co/yedam/board/CommentDAO.java
+++ welcomeHome1/src/main/java/co/yedam/board/CommentDAO.java
@@ -16,6 +16,27 @@ public class CommentDAO extends DAO {
 		instance = new CommentDAO();
 		return instance;
 	}
+	
+	public HashMap<String, Integer> getAmtByCountry(){
+		connect();
+		HashMap<String, Integer>map = new HashMap<String, Integer>();
+		String sql = "select billingCountry, sum(total) as amt from invoices i group by billingCountry";
+		try {
+			
+			pstmt = conn.prepareStatement(sql);
+			rs =pstmt.executeQuery();
+			while(rs.next()) {
+				map.put(rs.getString(1), rs.getInt(2));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			disconnect(); //호출시 락을 안생기게
+		}
+		return map;
+		
+	}
+	
 	//글삭제(매개값:글번호)
 	public HashMap<String, Object> delete(String id){
 		connect();
